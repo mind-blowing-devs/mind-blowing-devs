4793f8e93c67723c4fbe486f832654b8255680cc
ilyatitovich <ilyatitovich@gmail.com>
2025-02-25 15:54:52 +0100

    Add 404 page

---
+++ packages/client/src/pages/Error404/Error404.tsx
@@ -0,0 +1,11 @@
+export default function Error404Page() {
+  return (
+    <main>
+      <h1>404</h1>
+      <p>Упс! Страница не найдена.</p>
+      <a href="/" aria-label="Вернуться на главную страницу">
+        На главную
+      </a>
+    </main>
+  )
+}
---
+++ packages/client/src/pages/index.ts
@@ -0,0 +1,3 @@
+import Error404Page from './Error404'
+
+export { Error404Page }
