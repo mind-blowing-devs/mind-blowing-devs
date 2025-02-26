3a1cb7562c504d4812c4523e8c3d1bf00f54164b
p.artemev <p.artemev@systtech.ru>
2025-02-26 23:34:21 +0200

    Added base routes

---
+++ packages/client/src/pages/Error500/Error500.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center px-6 text-center">
+      <h1 className="text-6xl font-bold">500</h1>
+    </main>
+  )
+}
