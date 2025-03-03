2b1371d73e3ef00d2cb9c38368bd68f9c6ae5f6a
ilyatitovich <ilyatitovich@gmail.com>
2025-03-03 15:54:29 +0100

    Remove index.ts from page dir

--- packages/client/src/pages/Error404/Error404.tsx
+++ packages/client/src/pages/Error404/Error404.tsx
@@ -4,7 +4,7 @@ export default function Error404() {
   return (
     <main className="font-press bg-[#BFBFBF] flex h-screen flex-col items-center justify-center px-6 text-center">
       <h1 className="text-6xl font-bold">404</h1>
-      <p className="mt-4 text-xl">Oops! Page not found.</p>
+      <p className="mt-4 text-xl text-gray-700">Oops! Page not found.</p>
       <Link
         to="/"
         className="mt-8 hover:text-gray-500"
--- packages/client/src/pages/index.ts
+++
@@ -1,3 +0,0 @@
-import Error404Page from './Error404'
-
-export { Error404Page }
