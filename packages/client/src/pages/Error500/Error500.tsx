10d4b40cc411b9d5b2845c52885ec8e7b35e5a03
ilyatitovich <ilyatitovich@gmail.com>
2025-03-04 11:39:33 +0100

    Refactor design

--- packages/client/src/pages/Error500/Error500.tsx
+++ packages/client/src/pages/Error500/Error500.tsx
@@ -2,14 +2,26 @@ import { Link } from 'react-router-dom'
 
 export default function Error500() {
   return (
-    <main className="font-press bg-[#BFBFBF] flex h-screen flex-col items-center justify-center px-6 text-center">
-      <h1 className="text-6xl font-bold">500</h1>
-      <p className="mt-4 text-xl text-gray-700">Unexpected error. We're on it!</p>
+    <main className="font-press bg-[#BFBFBF] flex flex-col items-center justify-center min-h-screen p-4 sm:p-6">
+      <p className="w-full max-w-[250px] sm:max-w-xl mb-[42px] sm:mb-[72px] sm:text-xl text-center text-[#585656]">
+        Oops! Something exploded on the server!
+      </p>
+      <div className="border-4 border-[#818181] bg-[#D9D9D9] flex flex-col sm:flex-row justify-center items-center relative w-full max-w-xl h-[50vh]">
+        <img
+          className="absolute top-[-66px] sm:top-[-80px] sm:left-[-88px] left-[-50px] select-none"
+          src="/mine-icon.png"
+          width={176}
+          height={230}
+          alt="Mine Icon"
+        />
+        <h1 className="text-lg sm:text-xl font-bold">500</h1>
+        <p className="text-lg sm:text-xl font-bold">(Server Error)</p>
+      </div>
       <Link
         to="/"
-        className="mt-8 hover:text-gray-500"
+        className="mt-8 hover:text-gray-500 text-xs sm:text-sm"
         aria-label="Back to home page">
-        [Back Home]
+        [return home]
       </Link>
     </main>
   )
