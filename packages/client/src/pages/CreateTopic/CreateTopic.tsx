21124a91a4a260ee2d9384429e64fdb15eb52d6d
req-dev <uuserovich33@gmail.com>
2025-03-06 14:56:41 +1000

    devided the form on CreateTopic page into inputs

--- packages/client/src/pages/CreateTopic/CreateTopic.tsx
+++ packages/client/src/pages/CreateTopic/CreateTopic.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import TitleInput from './components/TitleInput'
+import DescriptionInput from './components/DescriptionInput'
+import CategoryInput from './components/CategoryInput'
 
 export default function CreateTopic() {
   return (
@@ -12,56 +15,17 @@ export default function CreateTopic() {
       </span>
 
       <div className="border-4 border-[#818181] bg-[#D9D9D9] flex flex-col justify-center items-center relative w-full lg:px-[32px] md:px-[15px] px-[10px] lg:h-[524px] md:h-[424px] h-[364px] text-xs md:text-base sm:text-sm overflow-y-auto">
-        <h3 className="font-roboto font-semibold w-full mb-[2px] sm:mb-[4px] lg:mb-[12px] text-lg lg:text-xl sm:text-lg text-black sm:text-center text-left">
+        <h3 className="font-roboto font-semibold w-full my-[2px] sm:my-[4px] lg:my-[12px] text-lg lg:text-xl sm:text-lg text-black sm:text-center text-left">
           Create New Topic
         </h3>
-        <span className="font-roboto w-full font-sm lg:text-lg sm:text-sm lg:mb-[60px] sm:mb-[30px] mb-[10px] text-black sm:text-center text-left">
+        <span className="font-roboto w-full font-sm lg:text-lg sm:text-sm sm:mb-[10px] mb-[5px] text-black sm:text-center text-left">
           Share your thoughts, ask questions or start a discussion!
         </span>
 
         <form className="w-full flex flex-col items-start">
-          <label
-            className="font-roboto font-semibold pt-[40px] pb-[12px]"
-            htmlFor="topicTitle">
-            Topic Title
-          </label>
-          <span>
-            [
-            <input
-              type="search"
-              id="topicTitle"
-              className="bg-transparent outline-none w-[180px] border-b-[1px] border-black border-dashed"
-            />
-            ]
-          </span>
-
-          <label
-            className="font-roboto font-semibold pt-[40px] pb-[12px]"
-            htmlFor="topicCategory">
-            Category (optional)
-          </label>
-          <span>
-            [
-            <select
-              className="bg-transparent outline-none w-[120px]"
-              id="topicCategory">
-              <option value="any">Any Category</option>
-              <option value="strategies">Strategies</option>
-              <option value="leaderboard">Leaderboard</option>
-              <option value="developers">Developers</option>
-              <option value="features">Features</option>
-            </select>
-            ]
-          </span>
-
-          <label
-            className="font-roboto font-semibold pt-[40px] pb-[12px]"
-            htmlFor="topicDescription">
-            Topic Description
-          </label>
-          <textarea
-            className="w-full h-auto min-h-[70px] max-h-[192px] border-2 border-[#818181] bg-[#D9D9D9] outline-none"
-            id="topicDescription"></textarea>
+          <TitleInput />
+          <CategoryInput />
+          <DescriptionInput />
         </form>
       </div>
 
