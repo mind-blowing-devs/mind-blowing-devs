c7d1d7d5123fe99efc683a057a2cae7de1833a1f
ilyatitovich <ilyatitovich@gmail.com>
2025-03-06 10:50:25 +0100

    Clean up profile actions

--- packages/client/src/pages/Profile/components/ProfileActions/ProfileActions.tsx
+++ packages/client/src/pages/Profile/components/ProfileActions/ProfileActions.tsx
@@ -1,41 +1,56 @@
+import { Link } from 'react-router-dom'
+
 type ProfileActionsProps = {
-  isEditPassword: boolean
+  isEdit: boolean
   setSearchParams: (props: Record<string, string>) => void
-  onLogOut?: () => Promise<void> | undefined
+  onLogOut?: () => Promise<void>
 }
 
 export default function ProfileActions({
-  isEditPassword,
+  isEdit,
   setSearchParams,
   onLogOut,
 }: ProfileActionsProps) {
-  const actions = isEditPassword
+  const actions = isEdit
     ? [{ text: '[back to profile]', onClick: () => setSearchParams({}) }]
     : [
         {
-          text: '[edit avatar]',
+          text: '[change avatar]',
           onClick: () => setSearchParams({ edit: 'avatar' }),
         },
         {
           text: '[change password]',
           onClick: () => setSearchParams({ edit: 'password' }),
         },
+        {
+          text: '[back to game]',
+          href: '/game',
+        },
         { text: '[log out]', onClick: onLogOut, className: 'text-red-600' },
       ]
 
   return (
     <div className="mt-8 flex flex-col justify-center items-center gap-4 max-w-md mx-auto">
-      {actions.map(({ text, onClick, className }, index) => (
-        <button
-          key={index}
-          type="button"
-          onClick={onClick}
-          className={`w-auto inline-block bg-transparent hover:opacity-80 text-xs sm:text-sm ${
-            className || ''
-          }`.trim()}>
-          {text}
-        </button>
-      ))}
+      {actions.map(({ text, onClick, href, className }) =>
+        href ? (
+          <Link
+            key={text}
+            to={href}
+            className="text-xs sm:text-sm hover:opacity-80">
+            {text}
+          </Link>
+        ) : (
+          <button
+            key={text}
+            type="button"
+            onClick={onClick}
+            className={`w-auto inline-block bg-transparent hover:opacity-80 text-xs sm:text-sm ${
+              className || ''
+            }`.trim()}>
+            {text}
+          </button>
+        )
+      )}
     </div>
   )
 }
