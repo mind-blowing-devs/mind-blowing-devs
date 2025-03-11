bf6d3a942bcac27f2f50396e8b56eb4dcacbf290
p.artemev <p.artemev@systtech.ru>
2025-03-11 20:44:14 +0200

    added ErrorBoundary component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -11,10 +11,11 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import CreateTopic from './pages/CreateTopic'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import { AuthProvider } from './hooks/useAuth'
 import NotAuthedProtectedRoutes from './components/NotAuthedProtectedRoutes'
 import AuthedProtectedRoutes from './components/AuthedProtectedRoutes'
+import ErrorBoundary from './components/ErrorBoundary'
 
 function App() {
   useEffect(() => {
@@ -27,6 +28,7 @@ function App() {
 
     fetchServerData()
   }, [])
+  const navigate = useNavigate()
   return (
     <div className="App">
       <nav>
@@ -49,29 +51,26 @@ function App() {
           ))}
         </ul>
       </nav>
-      <AuthProvider>
-        <Routes>
-          {/* <Route index element={
-            <ProtectedRoute>
-              <Main />
-            </ProtectedRoute>
-          }></Route> */}
-          <Route element={<NotAuthedProtectedRoutes />}>
-            <Route index element={<Main />}></Route>
-            <Route path="/forum" element={<Forum />}></Route>
-            <Route path="/forumtopic" element={<ForumTopic />}></Route>
-            <Route path="/game" element={<Game />}></Route>
-            <Route path="/leaderboard" element={<Leaderboard />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-          </Route>
-          <Route element={<AuthedProtectedRoutes />}>
-            <Route path="/signUp" element={<SignUp />}></Route>
-            <Route path="/signin" element={<SignIn />}></Route>
-          </Route>
-          <Route path="*" element={<Error404 />}></Route>
-          <Route path="/500" element={<Error500 />}></Route>
-        </Routes>
-      </AuthProvider>
+      <ErrorBoundary navigate={navigate}>
+        <AuthProvider>
+          <Routes>
+            <Route element={<NotAuthedProtectedRoutes />}>
+              <Route index element={<Main />}></Route>
+              <Route path="/forum" element={<Forum />}></Route>
+              <Route path="/forumtopic" element={<ForumTopic />}></Route>
+              <Route path="/game" element={<Game />}></Route>
+              <Route path="/leaderboard" element={<Leaderboard />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+            </Route>
+            <Route element={<AuthedProtectedRoutes />}>
+              <Route path="/signUp" element={<SignUp />}></Route>
+              <Route path="/signin" element={<SignIn />}></Route>
+            </Route>
+            <Route path="*" element={<Error404 />}></Route>
+            <Route path="/500" element={<Error500 />}></Route>
+          </Routes>
+        </AuthProvider>
+      </ErrorBoundary>
     </div>
   )
 }
