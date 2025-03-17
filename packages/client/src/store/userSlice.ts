57be18e1342a227bcf7759ce153c6e0bd2dcf1d0
ilyatitovich <ilyatitovich@gmail.com>
2025-03-17 16:46:05 +0100

    Add user slice in redux

---
+++ packages/client/src/store/userSlice.ts
@@ -0,0 +1,44 @@
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from '@reduxjs/toolkit'
+import { userAPI, type User } from '../api/userAPI'
+
+
+type UserState = {
+  user: User | null
+}
+
+const initialState: UserState = {
+  user: null,
+}
+
+export const changeAvatar = createAsyncThunk(
+  'user/changeAvatar',
+  async (data: FormData, { rejectWithValue }) => {
+    try {
+      const user = await userAPI.changeAvatar(data)
+      return user
+    } catch (error) {
+      return rejectWithValue('Error durring avatar change')
+    }
+  }
+)
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User | null>) => {
+      state.user = action.payload
+    },
+  },
+  extraReducers: builder =>
+    builder.addCase(changeAvatar.fulfilled, (state, action) => {
+      state.user = action.payload
+    }),
+})
+
+export const { setUser } = userSlice.actions
+export default userSlice.reducer
