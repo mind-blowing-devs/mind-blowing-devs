9ff5b6fd256036bbc9a1278f00936eb93b9cc7aa
Ilya Titov <ilyatitovdev@gmail.com>
2025-04-12 10:32:57 +0200

    Move service worker to useEffect and remove window load event listener

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -45,30 +45,26 @@ function App() {
     }
   }, [])
 
-  const startServiceWorker = () => {
+  useEffect(() => {
     if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker
-          .register('/service-worker.js')
-          .then(registration => {
-            console.log(
-              'ServiceWorker registration successful with scope: ',
-              registration.scope
-            )
-          })
-          .catch((error: string) => {
-            console.log('ServiceWorker registration failed: ', error)
-          })
-      })
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          )
+        })
+        .catch((error: string) => {
+          console.log('ServiceWorker registration failed: ', error)
+        })
     }
-  }
+  }, [])
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen)
   }
-
-  startServiceWorker()
 
   const navigate = useNavigate()
   const { isLogged } = useAuth()
