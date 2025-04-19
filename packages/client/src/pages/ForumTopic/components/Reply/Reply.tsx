de0493aa03bb037de14bbcced66dd6fb9b5ab83d
Sergey <yapikay@ya.ru>
2025-04-19 21:57:59 +0300

    feat: Add EmojiReactions to Reply component

--- packages/client/src/pages/ForumTopic/components/Reply/Reply.tsx
+++ packages/client/src/pages/ForumTopic/components/Reply/Reply.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import { EmojiReactions } from '../../../../components/EmojiReactions'
 
 export interface IReply {
   nickname: string
   text: string
   timestamp: string
+  id?: number
 }
 
-export default function Reply({ nickname, text, timestamp }: IReply) {
+export default function Reply({ nickname, text, timestamp, id = 0 }: IReply) {
   return (
-    <div>
+    <div className="mb-4 pb-4 border-b border-gray-200">
       <div className="font-roboto font-normal mb-[12px]">
         <span className="underline decoration-solid">{nickname}</span> •{' '}
         {timestamp}
       </div>
-      <span className='font-roboto'>{text}</span>
+      <span className="font-roboto">{text}</span>
+      <EmojiReactions replyId={id} />
     </div>
   )
 }
