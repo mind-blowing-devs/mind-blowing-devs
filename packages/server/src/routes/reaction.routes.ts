b82e7f6d63836df3722977663d040f9c026cd293
Sergey <yapikay@ya.ru>
2025-04-21 01:29:42 +0300

    feat: Add reaction routes

---
+++ packages/server/src/routes/reaction.routes.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import {
+  getReactionsController,
+  addReactionController,
+  removeReactionController,
+} from '../controllers'
+import { validateRequestData } from '../middlewares'
+import {
+  getReactionsSchema,
+  addReactionSchema,
+  removeReactionSchema,
+} from '../schemas'
+
+const router = express.Router()
+
+router.get(
+  '/',
+  validateRequestData(getReactionsSchema, 'query'),
+  getReactionsController
+)
+
+router.post(
+  '/',
+  validateRequestData(addReactionSchema, 'body'),
+  addReactionController
+)
+
+router.delete(
+  '/:id',
+  validateRequestData(removeReactionSchema, 'params'),
+  removeReactionController
+)
+
+export default router
