7a08fd004812f997a36d51e0bbdbfb08895791de
p.artemev <p.artemev@systtech.ru>
2025-04-21 19:40:06 +0200

    updated errors constants

--- packages/server/src/constants/errors.ts
+++ packages/server/src/constants/errors.ts
@@ -2,10 +2,14 @@ export const TOPIC_NOT_FOUND = 'TOPIC_NOT_FOUND'
 export const COMMENT_NOT_FOUND = 'COMMENT_NOT_FOUND'
 export const REPLY_NOT_FOUND = 'REPLY_NOT_FOUND'
 export const PARENT_REPLY_NOT_FOUND = 'PARENT_REPLY_NOT_FOUND'
+export const VISUAL_THEME_NOT_FOUND = 'VISUAL_THEME_NOT_FOUND'
+export const USER_NOT_FOUND = 'USER_NOT_FOUND'
 
 export const NOT_FOUND_MESSAGES = {
   [TOPIC_NOT_FOUND]: 'Topic not found',
   [COMMENT_NOT_FOUND]: 'Comment not found',
   [REPLY_NOT_FOUND]: 'Reply not found',
   [PARENT_REPLY_NOT_FOUND]: 'Parent reply not found',
+  [VISUAL_THEME_NOT_FOUND]: 'Visual theme not found',
+  [USER_NOT_FOUND]: 'User not found',
 }
