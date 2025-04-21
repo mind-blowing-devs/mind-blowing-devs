438876cc7d67750bab3a6c03245b0ea80c2cad35
p.artemev <p.artemev@systtech.ru>
2025-04-21 19:31:25 +0200

    added VisualTheme models and auxiliary models

---
+++ packages/server/src/models/visualTheme.model.ts
@@ -0,0 +1,28 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  HasMany,
+} from 'sequelize-typescript'
+import { v4 as uuidv4 } from 'uuid'
+import { UserVisualTheme } from './userVisualTheme.model'
+
+@Table
+export class VisualTheme extends Model {
+  @PrimaryKey
+  @Default(uuidv4)
+  @Column({ type: DataType.UUID })
+  override id!: string
+
+  @Column({ type: DataType.TEXT, allowNull: false, unique: true })
+  name!: string
+
+  @Column({ type: DataType.JSON, allowNull: true })
+  settings?: string
+
+  @HasMany(() => UserVisualTheme)
+  userVisualTheme!: UserVisualTheme
+}
