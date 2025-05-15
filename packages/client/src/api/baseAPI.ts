49214e038f78f4fa4378fe855291adf619bf4c7f
Sergey <yapikay@ya.ru>
2025-05-15 18:06:19 +0300

    Fix: Replace hardcoded server URL with environment variable

--- packages/client/src/api/baseAPI.ts
+++ packages/client/src/api/baseAPI.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const API_URL = 'http://localhost:5001/api'
+const serverUrl = process.env.EXTERNAL_SERVER_URL || 'http://localhost:5001'
+export const API_URL = `${serverUrl}/api`
 
 export const $axios = axios.create({
   baseURL: API_URL,
