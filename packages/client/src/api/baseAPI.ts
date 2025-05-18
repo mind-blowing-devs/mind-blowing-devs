fc9847d54d9e80b98e3418a34309788c1157f4b1
req-dev <uuserovich33@gmail.com>
2025-05-18 18:49:52 +1000

    Added dynamic backend's URL injection

--- packages/client/src/api/baseAPI.ts
+++ packages/client/src/api/baseAPI.ts
@@ -1,6 +1,26 @@
 import axios from 'axios'
 
-export const API_URL = 'http://localhost:5001/api'
+declare global {
+  interface Window {
+    __API_URL__?: string
+  }
+}
+
+const getApiUrl = () => {
+  if (typeof window !== 'undefined') {
+    if (window.__API_URL__) {
+      return `${window.__API_URL__}/api`
+    }
+    // fallback for dev
+    return `http://${window.location.hostname}:5001/api`
+  }
+  // SSR fallback
+  return process.env.NODE_ENV === 'production'
+    ? 'http://server:5001' // refers to docker container
+    : 'http://localhost:5001/api'
+}
+
+export const API_URL = getApiUrl()
 
 export const $axios = axios.create({
   baseURL: API_URL,
